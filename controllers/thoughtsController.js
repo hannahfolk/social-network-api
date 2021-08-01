9413adfc77d9595ce956345d5fabe890f71c11fb
hfolk25 <hfolk25@gmail.com>
2021-08-01 00:55:46 -0700

    finished controllers

---
+++ controllers/thoughtsController.js
@@ -0,0 +1,88 @@
+const { Thought } = require("../models");
+
+const getAllThoughts = async (req, res) => {
+  try {
+    const thoughts = await Thought.find();
+    console.log(thoughts)
+    res.status(200).json(thoughts);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+const getThoughtById = async (req, res) => {
+  try {
+    const { thoughtId } = req.params;
+    const thought = await Thought.findById(thoughtId);
+    res.status(200).json(thought);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+const createThought = async (req, res) => {
+  try {
+    const { body } = req;
+    const thought = await Thought.create(body);
+    res.status(200).json(thought);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+};
+
+const updateThought = async (req, res) => {
+  try {
+    const { thoughtId } = req.params;
+    const { body } = req;
+    const thought = await Thought.findByIdAndUpdate(thoughtId, body);
+    res.status(200).json(thought);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+};
+
+const deleteThought = async (req, res) => {
+  try {
+    const { thoughtId } = req.params;
+    const thought = await Thought.findByIdAndDelete(thoughtId);
+    res.status(200).json(thought);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+};
+
+const addReaction = async (req, res) => {
+  try {
+    const { thoughtId } = req.params;
+    const { body } = req;
+    const thought = await Thought.findByIdAndUpdate(thoughtId, {
+      $push: { reactions: body },
+    });
+    res.status(200).json(thought);
+  } catch (err) {
+    console.log(err);
+    res.status(400).json(err);
+  }
+};
+
+const deleteReaction = async (req, res) => {
+  try {
+    const { thoughtId, reactionId } = req.params;
+    const thought = await Thought.findByIdAndUpdate(thoughtId, {
+      $pull: { reactions: { reactionId } },
+    });
+    res.status(200).json(thought);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+};
+
+module.exports = {
+  getAllThoughts,
+  getThoughtById,
+  createThought,
+  updateThought,
+  deleteThought,
+  addReaction,
+  deleteReaction,
+};
---
+++ controllers/usersController.js
@@ -0,0 +1,85 @@
+const { User } = require("../models");
+
+const getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.status(200).json(users);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+const getUserById = async (req, res) => {
+  try {
+    const { userId } = req.params;
+    const user = await User.findById(userId);
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+const createUser = async (req, res) => {
+  try {
+    const { body } = req;
+    const user = await User.create(body);
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+};
+
+const updateUser = async (req, res) => {
+  try {
+    const { userId } = req.params;
+    const { body } = req;
+    const user = await User.findByIdAndUpdate(userId, body);
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+};
+
+const deleteUser = async (req, res) => {
+  try {
+    const { userId } = req.params;
+    const user = await User.findByIdAndDelete(userId);
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+};
+
+const addFriend = async (req, res) => {
+  try {
+    const { userId, friendId } = req.params;
+    const user = await User.findByIdAndUpdate(userId, {
+      $push: { friends: friendId },
+    });
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+};
+
+const deleteFriend = async (req, res) => {
+  try {
+    const { userId, friendId } = req.params;
+    const user = await User.findByIdAndUpdate(userId, {
+      $pull: { friends: friendId },
+    });
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+};
+
+module.exports = {
+  getAllUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
+  addFriend,
+  deleteFriend,
+};
