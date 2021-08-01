387702e49ff9aea27052a82862cebd6e1c1788dd
hfolk25 <hfolk25@gmail.com>
2021-08-01 00:55:38 -0700

    modified moved route methods around to match criteria

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -10,10 +10,10 @@ const {
 } = require("../../controllers/thoughtsController");
 
 // /api/thoughts
-router.route("/").get(getAllThoughts).get(getThoughtById).post(createThought);
+router.route("/").get(getAllThoughts).post(createThought);
 
 // /api/thoughts/:thoughtId
-router.route("/:thoughtId").put(updateThought).delete(deleteThought);
+router.route("/:thoughtId").get(getThoughtById).put(updateThought).delete(deleteThought);
 
 // /api/thoughts/:thoughtId/reactions
 router.route("/:thoughtId/reactions").post(addReaction);
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -10,15 +10,12 @@ const {
 } = require("../../controllers/usersController");
 
 // /api/users
-router.route("/").get(getAllUsers).get(getUserById).post(createUser);
+router.route("/").get(getAllUsers).post(createUser);
 
 // /api/users/:userId
-router.route("/:userId").put(updateUser).delete(deleteUser);
-
-// /api/users/:userId/friends
-router.route("/:userId/friends").post(addFriend);
+router.route("/:userId").get(getUserById).put(updateUser).delete(deleteUser);
 
 // /api/users/:userId/friends/:friendId
-router.route("/:userId/friends/:friendId").delete(deleteFriend);
+router.route("/:userId/friends/:friendId").post(addFriend).delete(deleteFriend);
 
 module.exports = router;
