9ea138d2253b8fe0520346adea4097d6d8718589
hfolk25 <hfolk25@gmail.com>
2021-08-01 02:31:25 -0700

    added thoughtId into user's thoughts array when creating a thought, and remove it when deleting a thought

--- controllers/thoughtsController.js
+++ controllers/thoughtsController.js
@@ -1,9 +1,8 @@
-const { Thought } = require("../models");
+const { User, Thought } = require("../models");
 
 const getAllThoughts = async (req, res) => {
   try {
     const thoughts = await Thought.find();
-    console.log(thoughts)
     res.status(200).json(thoughts);
   } catch (err) {
     res.status(500).json(err);
@@ -24,8 +23,15 @@ const createThought = async (req, res) => {
   try {
     const { body } = req;
     const thought = await Thought.create(body);
+    await User.findOneAndUpdate(
+      { username: body.username },
+      {
+        $push: { thoughts: thought._id },
+      }
+    );
     res.status(200).json(thought);
   } catch (err) {
+    console.log(err);
     res.status(400).json(err);
   }
 };
@@ -45,6 +51,10 @@ const deleteThought = async (req, res) => {
   try {
     const { thoughtId } = req.params;
     const thought = await Thought.findByIdAndDelete(thoughtId);
+    await User.findOneAndUpdate(
+      { username: thought.username },
+      { $pull: { thoughts: thought._id } }
+    );
     res.status(200).json(thought);
   } catch (err) {
     res.status(400).json(err);
